1863576128acd3102fbff057ac28b852742db834
Vladimir Zarypov <krre31@gmail.com>
2015-06-11 14:24:40 +0300

    placed main window in center of screen

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,17 @@
+#include "QtCore"
+#include <QScreen>
+#include <QGuiApplication>
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
-{
-    resize(800, 600);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setWindowTitle("Greenery");
+
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QSize screenSize = screen->size();
+    int width = 800;
+    int height = 600;
+    int x = (screenSize.width() - width) / 2;
+    int y = (screenSize.height() - height) / 2;
+    setGeometry(x, y, width, height);
 }
 
