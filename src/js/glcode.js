9dd9bf5b077f2564bcd9fde70a021b555297be17
Vladimir Zarypov <krre31@gmail.com>
2015-06-15 14:56:41 +0300

    added clearing canvas by three.js

--- src/js/glcode.js
+++ src/js/glcode.js
@@ -1,59 +1,40 @@
 Qt.include("three.js")
-//var gl
 
  var camera, scene, renderer;
  var geometry, material, mesh;
 
  function initializeGL(canvas) {
+    camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 1, 10000 );
+    camera.position.z = 1000;
 
-     camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 1, 10000 );
-     camera.position.z = 1000;
+    scene = new THREE.Scene();
 
-     scene = new THREE.Scene();
+    geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
-     geometry = new THREE.BoxGeometry( 200, 200, 200 );
-     material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
 
-     mesh = new THREE.Mesh( geometry, material );
-     scene.add( mesh );
-
-     renderer = new THREE.Canvas3DRenderer( {canvas: canvas, devicePixelRatio: canvas.devicePixelRatio});
-     renderer.setSize( canvas.width, canvas.height );
+    renderer = new THREE.Canvas3DRenderer( {canvas: canvas, devicePixelRatio: canvas.devicePixelRatio});
+    renderer.setSize( canvas.width, canvas.height );
+    renderer.setClearColor(new THREE.Color(0.19, 0.12, 0.08) , 1)
  }
 
  function paintGL(canvas) {
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
 
-     mesh.rotation.x += 0.01;
-     mesh.rotation.y += 0.02;
-
-     renderer.render( scene, camera );
-
+    renderer.clear()
+    renderer.render( scene, camera );
  }
 
  function resizeGL(canvas) {
+    if (camera === undefined) return;
 
-     if (camera === undefined) return;
+    camera.aspect = canvas.width / canvas.height;
+    camera.updateProjectionMatrix();
 
-     camera.aspect = canvas.width / canvas.height;
-     camera.updateProjectionMatrix();
-
-     renderer.setSize( canvas.width, canvas.height );
-
+    renderer.setSize( canvas.width, canvas.height );
  }
 
-//function initializeGL(canvas) {
-//    canvas3d = canvas
-//    try {
-//        gl = canvas.getContext("canvas3d")
-//        gl.clearColor(0.19, 0.12, 0.08, 1.0)
-//    } catch(e) {
-//        print("initializeGL FAILURE!")
-//        print(""+e)
-//        print(""+e.message)
-//    }
-//}
-
-//function paintGL(canvas) {
-//    gl.clear(gl.COLOR_BUFFER_BIT)
-//}
 
