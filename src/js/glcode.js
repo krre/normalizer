cbcec51241d1b90ed9334bae67b4dba274fadc57
Vladimir Zarypov <krre31@gmail.com>
2015-06-17 12:22:43 +0300

    removed semicolon from glcode.js

--- src/js/glcode.js
+++ src/js/glcode.js
@@ -1,40 +1,40 @@
 Qt.include("three.js")
 
- var camera, scene, renderer;
- var geometry, material, mesh;
+ var camera, scene, renderer
+ var geometry, material, mesh
 
  function initializeGL(canvas) {
-    camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 1, 10000);
-    camera.position.z = 10;
+    camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 1, 10000)
+    camera.position.z = 10
 
-    scene = new THREE.Scene();
+    scene = new THREE.Scene()
 
-    geometry = new THREE.BoxGeometry(1, 1, 1);
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    geometry = new THREE.BoxGeometry(1, 1, 1)
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
 
-    renderer = new THREE.Canvas3DRenderer({canvas: canvas, devicePixelRatio: canvas.devicePixelRatio});
-    renderer.setSize(canvas.width, canvas.height);
+    renderer = new THREE.Canvas3DRenderer({canvas: canvas, devicePixelRatio: canvas.devicePixelRatio})
+    renderer.setSize(canvas.width, canvas.height)
     renderer.setClearColor(new THREE.Color(0.19, 0.12, 0.08), 1)
  }
 
  function paintGL(canvas) {
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.02
 
     renderer.clear()
-    renderer.render(scene, camera);
+    renderer.render(scene, camera)
  }
 
  function resizeGL(canvas) {
-    if (camera === undefined) return;
+    if (camera === undefined) return
 
-    camera.aspect = canvas.width / canvas.height;
-    camera.updateProjectionMatrix();
+    camera.aspect = canvas.width / canvas.height
+    camera.updateProjectionMatrix()
 
-    renderer.setSize(canvas.width, canvas.height);
+    renderer.setSize(canvas.width, canvas.height)
  }
 
 
