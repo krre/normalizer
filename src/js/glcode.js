5af4fe74912b9e13af1f9d3b2465c6b06d3d7292
Vladimir Zarypov <krre31@gmail.com>
2015-06-18 14:02:34 +0300

    added AxisHelper

--- src/js/glcode.js
+++ src/js/glcode.js
@@ -17,6 +17,9 @@ function initializeGL(canvas, eventSource) {
 
     scene = new THREE.Scene()
 
+    var axisHelper = new THREE.AxisHelper(5)
+    scene.add(axisHelper)
+
     geometry = new THREE.BoxGeometry(1, 1, 1)
     material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
