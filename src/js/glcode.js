bb75a0d334e4d0c08714e37a0b61b66a6ea5ea28
Vladimir Zarypov <krre31@gmail.com>
2015-06-19 12:09:15 +0300

    root items is linked by lines

--- src/js/glcode.js
+++ src/js/glcode.js
@@ -51,43 +51,66 @@ function loadModel(model) {
     if (rootItemCount) {
         var step = Math.PI * 2 / rootItemCount
         var radius = 2
+        var startLinePos
+        var firstListPos
         for (var i in model) {
             var x = radius * Math.cos(step * i)
             var z = radius * Math.sin(step * i)
             var item
+            var origin = new THREE.Vector3(0, 0, 0)
+            var itemPos = new THREE.Vector3(x, 0, z)
             if (typeof model[i] === "object") {
-                item = createSphere(x, 0, z)
+                item = createSphere(itemPos)
+                addBranch(model[i], origin, itemPos)
             } else {
-                item = createBox(x, 0, z)
+                item = createBox(itemPos)
             }
-            item.lookAt(new THREE.Vector3(0, 0, 0))
+            item.lookAt(origin)
             scene.add(item)
+            if (!firstListPos) {
+                firstListPos = itemPos
+            }
+
+            if (startLinePos) {
+                scene.add(createLine(startLinePos, itemPos))
+            }
+            startLinePos = itemPos
         }
+        scene.add(createLine(itemPos, firstListPos))
     }
 }
 
-function addBranch(branch) {
-
+function addBranch(branch, origin, direction) {
+//    new THREE.Ray(origin, new THREE.Vector3(x, 0, z).normalize())
+    print(JSON.stringify(origin))
 }
 
-function createBox(x, y, z) {
+function createBox(pos) {
     var geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize)
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
     var box = new THREE.Mesh(geometry, material)
-    box.position.x = x
-    box.position.y = y
-    box.position.z = z
+    box.position.x = pos.x
+    box.position.y = pos.y
+    box.position.z = pos.z
     return box
 }
 
-function createSphere(x, y, z) {
+function createSphere(pos) {
     var geometry = new THREE.SphereGeometry(sphereSize)
     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
     var sphere = new THREE.Mesh(geometry, material)
-    sphere.position.x = x
-    sphere.position.y = y
-    sphere.position.z = z
+    sphere.position.x = pos.x
+    sphere.position.y = pos.y
+    sphere.position.z = pos.z
     return sphere
+}
+
+function createLine(startPos, endPos) {
+    var material = new THREE.LineBasicMaterial({ color: 0xff000ff })
+    var geometry = new THREE.Geometry()
+    geometry.vertices.push(startPos, endPos)
+    var line = new THREE.Line(geometry, material)
+    return line
 }
 
 
