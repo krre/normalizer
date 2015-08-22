83e5b54dd431b56f5892bd46f752cfdf359be009
Vladimir Zarypov <krre31@gmail.com>
2015-08-22 10:26:20 +0300

    added "body" field in Functions table

--- src/cpp/SproutDb.cpp
+++ src/cpp/SproutDb.cpp
@@ -30,7 +30,7 @@ void SproutDb::initDb()
 {
     QSqlQuery query(db);
     query.exec("CREATE TABLE Defs(version)");
-    query.exec("CREATE TABLE Functions(name)");
+    query.exec("CREATE TABLE Functions(name, body)");
     bool result = query.exec("INSERT INTO Defs (version) VALUES ('0.1.0')");
     if (!result) {
 
