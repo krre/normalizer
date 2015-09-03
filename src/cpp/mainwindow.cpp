696cbc70098af652dc9d1e34712adf48012f5da9
Vladimir Zarypov <krre31@gmail.com>
2015-09-03 11:06:03 +0300

    main window is moved to center screen on creating

--- src/cpp/mainwindow.cpp
+++ src/cpp/mainwindow.cpp
@@ -1,7 +1,18 @@
 #include "mainwindow.h"
+#include <QApplication>
+#include <QtCore/QRect>
+#include <QDesktopWidget>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     setWindowTitle("Greenery");
+
+    // move window to center screen
+    QRect desktopSize = QApplication::desktop()->screenGeometry();
+    int x = (desktopSize.width() - size().width()) / 2;
+    int y = (desktopSize.height() - size().height()) / 2;
+    move(x, y);
+
 }
 
