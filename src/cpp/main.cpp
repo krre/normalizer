fb8edb0ca79789f6cc2986d5644cc3807201e7f3
Vladimir Zarypov <krre31@gmail.com>
2015-09-15 15:41:09 +0300

    added command line parser

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -11,6 +11,23 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+    app.setApplicationName("Greenery");
+    QCoreApplication::setApplicationVersion("0.1.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("IDE for Sprout language");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("source", QCoreApplication::translate("main", "Sprout file to edit"));
+
+    parser.addOptions({
+        {{"p", "plane"}, QCoreApplication::translate("main", "Plain mode")},
+    });
+
+    parser.process(app);
+
+//    bool isPlain = parser.isSet("plane");
+//    qDebug() << isPlain;
 
     qmlRegisterType<Console>("Greenery.Lib", 1, 0, "Console");
     qmlRegisterType<SproutDb>("Greenery.Lib", 1, 0, "SproutDb");
