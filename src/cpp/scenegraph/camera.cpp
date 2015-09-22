6641db2ca72fe245a4221d8b71abf660ef6a056e
Vladimir Zarypov <krre31@gmail.com>
2015-09-22 10:36:45 +0300

    added 'color' property to Camera class

--- src/cpp/scenegraph/camera.cpp
+++ src/cpp/scenegraph/camera.cpp
@@ -5,3 +5,17 @@ Camera::Camera(QObject *parent) : QObject(parent)
 
 }
 
+QColor Camera::color() const
+{
+    return m_color;
+}
+
+void Camera::setColor(QColor color)
+{
+    if (m_color == color)
+        return;
+
+    m_color = color;
+    emit colorChanged(color);
+}
+
--- src/qml/WorkArea.qml
+++ src/qml/WorkArea.qml
@@ -76,6 +76,7 @@ Item {
 
     Camera {
         id: camera
+        color: Qt.rgba(1, 0, 0, 1)
     }
 
     Viewport {
