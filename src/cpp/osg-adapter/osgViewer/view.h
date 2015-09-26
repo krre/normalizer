d7742a2a01fde2f1a8db00f259de216107725b44
Vladimir Zarypov <krre31@gmail.com>
2015-09-26 20:59:53 +0300

    added osg::GraphicsContext in View class

--- src/cpp/osg-adapter/osgViewer/view.h
+++ src/cpp/osg-adapter/osgViewer/view.h
@@ -1,9 +1,19 @@
 #pragma once
+#include <QtCore>
 #include <QQuickItem>
+#include <osgViewer/View>
+#include <osg/Camera>
+#include <osg/GraphicsContext>
 
 class View : public QQuickItem
 {
     Q_OBJECT
 public:
     explicit View();
+private:
+    osgViewer::View* view;
+    osg::Camera* camera;
+    osg::GraphicsContext* graphicsContext;
+    void geometryChanged(const QRectF& newGeometry, const QRectF& oldGeometry) override;
+    QSGNode* updatePaintNode(QSGNode* oldNode, UpdatePaintNodeData*) override;
 };
