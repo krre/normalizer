ca8182aaa333490017fc6b5214cf66952e2c0a9a
Vladimir Zarypov <krre31@gmail.com>
2016-01-21 16:14:31 +0200

    added parent to message functions

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,18 +1,18 @@
 .import "../js/utils.js" as Utils
 
-function infoMessage(message) {
-    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogInformation.qml", { text: message })
+function infoMessage(message, parent) {
+    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/messagedialog/MessageDialogInformation.qml", { text: message })
 }
 
-function warningMessage(message) {
-    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogWarning.qml", { text: message })
+function warningMessage(message, parent) {
+    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/messagedialog/MessageDialogWarning.qml", { text: message })
 }
 
-function questionMessage(message) {
-    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogQuestion.qml", { text: message })
+function questionMessage(message, parent) {
+    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/messagedialog/MessageDialogQuestion.qml", { text: message })
 }
 
-function errorMessage(message) {
-    return Utils.createDynamicObject(mainRoot, "qrc:/qml/components/messagedialog/MessageDialogError.qml", { text: message })
+function errorMessage(message, parent) {
+    return Utils.createDynamicObject(parent || mainRoot, "qrc:/qml/components/messagedialog/MessageDialogError.qml", { text: message })
 }
 
