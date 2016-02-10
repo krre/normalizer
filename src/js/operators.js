92ae5837f5f82b4138655d17c9534a8b4cf74ede
Vladimir Zarypov <krre31@gmail.com>
2016-02-10 14:15:36 +0200

    added several new operators

--- src/js/operators.js
+++ src/js/operators.js
@@ -5,7 +5,12 @@ var operators = [
     { name: "Flow", shortcut: "F", action: flowOp },
     { name: "Add", shortcut: "Ctrl+A", action: addOp },
     { name: "Remove", shortcut: "Ctrl+R", action: removeOp },
-    { name: "Update", shortcut: "Ctrl+U", action: updateOp }
+    { name: "Update", shortcut: "Ctrl+U", action: updateOp },
+    { name: "Project", shortcut: "", action: projectOp },
+    { name: "Module", shortcut: "", action: moduleOp },
+    { name: "Function", shortcut: "", action: functionOp },
+    { name: "Expression", shortcut: "", action: expressionOp },
+    { name: "Condition", shortcut: "", action: conditionOp }
 ]
 
 function add() {
@@ -34,4 +39,24 @@ function removeOp() {
 
 function updateOp() {
     print("update")
+}
+
+function projectOp() {
+    print("project")
+}
+
+function moduleOp() {
+    print("module")
+}
+
+function functionOp() {
+    print("function")
+}
+
+function expressionOp() {
+    print("expression")
+}
+
+function conditionOp() {
+    print("condition")
 }
