ce469fcc385beab0fe9d1801af254ef2d37cf869
Vladimir Zarypov <krre31@gmail.com>
2016-02-22 12:16:33 +0200

    added shader compile functions in gl.js

--- src/js/webgl/gl.js
+++ src/js/webgl/gl.js
@@ -52,3 +52,73 @@ function paintGL(canvas) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
     scene.render()
 }
+
+function initShaders(gl, shaders) {
+    var program = createProgram(gl, shaders)
+    if (!program) {
+        print("Failed to create program")
+        return false
+    }
+
+    gl.useProgram(program)
+    gl.program = program
+
+    return true
+}
+
+function createProgram(gl, shaderSources) {
+    var shaders = []
+    for (var i = 0; i < shaderSources.length; i++) {
+        var shader = loadShader(gl, shaderSources[i].type, shaderSources[i].source)
+        if (shader) {
+            shaders.push(shader)
+        } else {
+            return null
+        }
+    }
+
+    var program = gl.createProgram()
+    if (!program) {
+        return null
+    }
+
+    for (i = 0; i < shaders.length; i++) {
+        gl.attachShader(program, shaders[i])
+    }
+
+    gl.linkProgram(program)
+
+    var linked = gl.getProgramParameter(program, gl.LINK_STATUS)
+    if (!linked) {
+        var error = gl.getProgramInfoLog(program)
+        print("Failed to link program: %1".arg(error))
+        gl.deleteProgram(program)
+        for (i = 0; i < shaders.length; i++) {
+            gl.deleteShader(program, shaders[i])
+        }
+        return null
+    }
+
+    return program
+}
+
+function loadShader(gl, type, source) {
+    var shader = gl.createShader(type)
+    if (!shader) {
+        print("Failed to create shader")
+    }
+
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    var compliled = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+    if (!compliled) {
+        var error = gl.getShaderInfoLog(shader)
+        print("Failed to compile shader: %1".arg(error))
+        gl.deleteShader(shader)
+        return null
+    }
+
+    return shader
+}
+
