42e47ef94874551e218974adf58f16bec9e460cc
Vladimir Zarypov <krre31@gmail.com>
2016-04-05 11:50:23 +0300

    added menu bar

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include <QtCore>
 #include <QtGui>
+#include <QApplication>
+#include <QMessageBox>
+#include <QMenuBar>
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
@@ -11,4 +14,78 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     int x = (screenSize.width() - width) / 2;
     int y = (screenSize.height() - height) / 2;
     setGeometry(x, y, width, height);
+
+    createActions();
+    createMenus();
+}
+
+void MainWindow::createActions() {
+    newAct = new QAction(tr("New"), this);
+    newAct->setShortcut(QKeySequence("Ctrl+N"));
+    connect(newAct, SIGNAL(triggered(bool)), this, SLOT(newFile()));
+
+    openAct = new QAction(tr("Open..."), this);
+    openAct->setShortcut(QKeySequence("Ctrl+O"));
+    connect(openAct, SIGNAL(triggered(bool)), this, SLOT(open()));
+
+    saveAct = new QAction(tr("Save"), this);
+    saveAct->setShortcut(QKeySequence("Ctrl+S"));
+    connect(saveAct, SIGNAL(triggered(bool)), this, SLOT(save()));
+
+    saveAsAct = new QAction(tr("Save As..."), this);
+    saveAsAct->setShortcut(QKeySequence("Ctrl+Shift+S"));
+    connect(saveAsAct, SIGNAL(triggered(bool)), this, SLOT(saveAs()));
+
+    quitAct = new QAction(tr("Exit"), this);
+    quitAct->setShortcut(QKeySequence("Ctrl+Q"));
+    connect(quitAct, SIGNAL(triggered(bool)), this, SLOT(quitApp()));
+
+    aboutAct = new QAction(tr("About..."), this);
+    connect(aboutAct, SIGNAL(triggered(bool)), this, SLOT(about()));
+
+    aboutQtAct = new QAction(tr("About Qt..."), this);
+    connect(aboutQtAct, SIGNAL(triggered(bool)), this, SLOT(aboutQt()));
+}
+
+void MainWindow::createMenus() {
+    fileMenu = menuBar()->addMenu(tr("File"));
+    fileMenu->addAction(newAct);
+    fileMenu->addAction(openAct);
+    fileMenu->addAction(saveAct);
+    fileMenu->addAction(saveAsAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(quitAct);
+
+    helpMenu = menuBar()->addMenu(tr("Help"));
+    helpMenu->addAction(aboutAct);
+    helpMenu->addAction(aboutQtAct);
+}
+
+void MainWindow::newFile() {
+    qDebug() << "new";
+}
+
+void MainWindow::open() {
+    qDebug() << "open";
+}
+
+void MainWindow::save() {
+    qDebug() << "save";
+}
+
+void MainWindow::saveAs() {
+    qDebug() << "save as";
+}
+
+void MainWindow::quitApp() {
+    QApplication::quit();
+}
+
+void MainWindow::about() {
+    QMessageBox::about(this, tr("About Greenery..."),
+            tr("<b>Greenery</b>"));
+}
+
+void MainWindow::aboutQt() {
+    QMessageBox::aboutQt(this);
 }
--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -1,9 +1,34 @@
 #pragma once
 #include <QMainWindow>
+#include <QAction>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget* parent = 0);
+
+private slots:
+    void newFile();
+    void open();
+    void save();
+    void saveAs();
+    void quitApp();
+    void about();
+    void aboutQt();
+
+private:
+    void createActions();
+    void createMenus();
+
+    QMenu *fileMenu;
+    QMenu *helpMenu;
+
+    QAction* newAct;
+    QAction* openAct;
+    QAction* saveAct;
+    QAction* saveAsAct;
+    QAction* quitAct;
+    QAction* aboutAct;
+    QAction* aboutQtAct;
 };
