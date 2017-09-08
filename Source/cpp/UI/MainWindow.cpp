516bd6352712c126c8ab78e39d0548b84ccf8b88
Vladimir Zarypov <krre31@gmail.com>
2017-09-08 14:32:15 +0300

    Refactor code format

--- Source/cpp/UI/MainWindow.cpp
+++ Source/cpp/UI/MainWindow.cpp
@@ -24,6 +24,6 @@ void MainWindow::on_actionAbout_triggered(bool checked) {
            Build on %4<br><br> \
            <a href=\"https://github.com/krre/sprout-editor\"> \
            https://github.com/krre/sprout-editor</a><br><br> \
-           Copyright © 2015-2017, Vladimir Zarypov \
-    ").arg(QApplication::applicationName()).arg(QApplication::applicationVersion()).arg(QT_VERSION_STR).arg(__DATE__));
+           Copyright © 2015-2017, Vladimir Zarypov").
+        arg(QApplication::applicationName()).arg(QApplication::applicationVersion()).arg(QT_VERSION_STR).arg(__DATE__));
 }
