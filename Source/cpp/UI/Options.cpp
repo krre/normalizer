21755b20e7f4655fc6086d2cbcae5b2a05c76589
Vladimir Zarypov <krre31@gmail.com>
2017-09-24 03:38:12 +0300

    Options dialog is saved and readed settings

--- Source/cpp/UI/Options.cpp
+++ Source/cpp/UI/Options.cpp
@@ -2,6 +2,7 @@
 #include "ui_Options.h"
 #include "../Defines.h"
 #include <QtCore>
+#include <QtWidgets>
 
 Options::Options(QWidget* parent) :
         QDialog(parent),
@@ -10,8 +11,41 @@ Options::Options(QWidget* parent) :
     ui->setupUi(this);
     adjustSize();
     resize(500, height());
+    readSettings();
 }
 
 Options::~Options() {
     delete ui;
+}
+
+void Options::on_pushButtonWorkspace_clicked() {
+    QString dirPath = QFileDialog::getExistingDirectory(this);
+    ui->lineEditWorkspace->setText(dirPath);
+}
+
+void Options::on_pushButtonCompiler_clicked() {
+    QString filePath = QFileDialog::getOpenFileName(this, tr("Select Sprout compiler"));
+    ui->lineEditCompiler->setText(filePath);
+}
+
+void Options::on_buttonBox_accepted() {
+    writeSettings();
+}
+
+void Options::readSettings() {
+    settings.beginGroup("Path");
+    ui->lineEditWorkspace->setText(settings.value("workspace").toString());
+    ui->lineEditCompiler->setText(settings.value("sproutCompiler").toString());
+    settings.endGroup();
+
+    ui->checkBoxSession->setChecked(settings.value("MainWindow/restoreSession").toBool());
+}
+
+void Options::writeSettings() {
+    settings.beginGroup("Path");
+    settings.setValue("workspace", ui->lineEditWorkspace->text());
+    settings.setValue("sproutCompiler", ui->lineEditCompiler->text());
+    settings.endGroup();
+
+    settings.setValue("MainWindow/restoreSession", ui->checkBoxSession->isChecked());
 }
--- Source/cpp/UI/Options.h
+++ Source/cpp/UI/Options.h
@@ -13,7 +13,15 @@ public:
     explicit Options(QWidget *parent = 0);
     ~Options();
 
+private slots:
+    void on_pushButtonWorkspace_clicked();
+    void on_pushButtonCompiler_clicked();
+    void on_buttonBox_accepted();
+
 private:
     Ui::Options *ui;
     QSettings settings;
+
+    void readSettings();
+    void writeSettings();
 };
