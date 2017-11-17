81d7deb52123d366a6cfa4d742fb60d71f31acdb
Vladimir Zarypov <krre31@gmail.com>
2017-11-17 16:54:57 +0200

    Set version of application in Qt project file

--- Source/Core/Defines.h
+++ Source/Core/Defines.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define APP_NAME "IrbisCave"
-#define APP_VERSION_STR "0.1.0"
+#define APP_VERSION_STR VERSION_STR
 #define APP_URL "https://github.com/krre/irbiscave"
 #define APP_SETTINGS_NAME "irbiscave.ini"
 #define WORKSPACE_DIRECTORY ".irbis"
