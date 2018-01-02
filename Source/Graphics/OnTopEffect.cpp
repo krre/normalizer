d3c646839085d75830a7eff0cc286f5114ed50bc
Vladimir Zarypov <krre31@gmail.com>
2018-01-02 07:16:54 +0200

    Remove OpenGLES support from OnTopEffect class

--- Source/Graphics/OnTopEffect.cpp
+++ Source/Graphics/OnTopEffect.cpp
@@ -9,31 +9,22 @@ OnTopEffect::OnTopEffect(Qt3DCore::QNode* parent) : Qt3DRender::QEffect(parent) 
     Qt3DRender::QTechnique* technique = new Qt3DRender::QTechnique();
     technique->graphicsApiFilter()->setProfile(Qt3DRender::QGraphicsApiFilter::NoProfile);
     technique->graphicsApiFilter()->setApi(Qt3DRender::QGraphicsApiFilter::OpenGL);
-    technique->graphicsApiFilter()->setMajorVersion(2);
-    technique->graphicsApiFilter()->setMinorVersion(1);
-
-    Qt3DRender::QTechnique* techniqueES2 = new Qt3DRender::QTechnique();
-    techniqueES2->graphicsApiFilter()->setApi(Qt3DRender::QGraphicsApiFilter::OpenGLES);
-    techniqueES2->graphicsApiFilter()->setMajorVersion(2);
-    techniqueES2->graphicsApiFilter()->setMinorVersion(0);
-    techniqueES2->graphicsApiFilter()->setProfile(Qt3DRender::QGraphicsApiFilter::NoProfile);
+    technique->graphicsApiFilter()->setMajorVersion(4);
+    technique->graphicsApiFilter()->setMinorVersion(3);
 
     Qt3DRender::QFilterKey* filterkey = new Qt3DRender::QFilterKey(this);
     filterkey->setName(QStringLiteral("renderingStyle"));
     filterkey->setValue(QStringLiteral("forward"));
 
     technique->addFilterKey(filterkey);
-    techniqueES2->addFilterKey(filterkey);
 
-    Qt3DRender::QShaderProgram *shader = new Qt3DRender::QShaderProgram();
+    Qt3DRender::QShaderProgram* shader = new Qt3DRender::QShaderProgram();
     shader->setVertexShaderCode(Qt3DRender::QShaderProgram::loadSource(QUrl(QStringLiteral("qrc:/Graphics/Shaders/OnTopMaterial.vert"))));
     shader->setFragmentShaderCode(Qt3DRender::QShaderProgram::loadSource(QUrl(QStringLiteral("qrc:/Graphics/Shaders/OnTopMaterial.frag"))));
 
     Qt3DRender::QRenderPass* renderPass = new Qt3DRender::QRenderPass();
     renderPass->setShaderProgram(shader);
     technique->addRenderPass(renderPass);
-    techniqueES2->addRenderPass(renderPass);
 
     addTechnique(technique);
-    addTechnique(techniqueES2);
 }
--- Source/Graphics/OnTopEffect.h
+++ Source/Graphics/OnTopEffect.h
@@ -1,10 +1,8 @@
 #pragma once
-
 #include <Qt3DRender/QEffect>
-#include <Qt3DRender/QTechnique>
 
 class OnTopEffect : public Qt3DRender::QEffect {
 
 public:
-    explicit OnTopEffect(Qt3DCore::QNode *parent = 0);
+    explicit OnTopEffect(Qt3DCore::QNode* parent = 0);
 };
