ff5ed542f5522b40d4dbcde415db27424a4998d0
Vladimir Zarypov <krre31@gmail.com>
2018-01-02 06:53:02 +0200

    Update copyright

--- Source/Core/Defines.h
+++ Source/Core/Defines.h
@@ -4,6 +4,7 @@
 #define APP_VERSION_STR VERSION_STR
 #define APP_URL "https://github.com/krre/irbiscave"
 #define APP_SETTINGS_NAME "irbiscave.ini"
+#define APP_COPYRIGHT "Copyright © 2017-2018, Vladimir Zarypov"
 #define WORKSPACE_DIRECTORY "IrbisProjects"
 #define PROJECT_DIRECTORY ".irbis"
 #define PROJECT_BUILD_DIRECTORY "build"
