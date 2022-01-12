86edb74ea2c57b7e13b6c5379d0bd541da959cb1
Vladimir Zarypov <krre31@gmail.com>
2022-01-12 07:32:49 +0200

    Move event loop and window instances to app instance

--- src/normalizer/core/app.rs
+++ src/normalizer/core/app.rs
@@ -1,31 +1,33 @@
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    window::{Window, WindowBuilder},
 };
 
-pub struct App;
+pub struct App {
+    event_loop: EventLoop<()>,
+    window: Window,
+}
 
 impl App {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
-        Ok(Self)
-    }
-
-    pub fn run(self) {
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new()
             .with_title("Normalizer")
-            .build(&event_loop)
-            .unwrap();
+            .build(&event_loop)?;
 
-        event_loop.run(move |event, _, control_flow| {
+        Ok(Self { event_loop, window })
+    }
+
+    pub fn run(self) {
+        self.event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Wait;
 
             match event {
                 Event::WindowEvent {
                     event: WindowEvent::CloseRequested,
                     window_id,
-                } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+                } if window_id == self.window.id() => *control_flow = ControlFlow::Exit,
                 _ => (),
             }
         });
