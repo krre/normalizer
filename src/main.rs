1e5dc678fd8d69c7ff63e80781dbb8739c41a01c
Vladimir Zarypov <krre31@gmail.com>
2022-01-30 11:45:37 +0200

    App instance is mutable

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use antiq::entity::Application;
 use antiq::widget::ApplicationWindow;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let app = Application::new();
+    let mut app = Application::new();
 
-    let mut app_window = ApplicationWindow::new(&app)?;
+    let mut app_window = ApplicationWindow::new(&mut app)?;
     app_window.set_title("Normalizer");
     app_window.set_visible(true);
+
+    app.add_window(app_window);
 
     app.run()
 }
