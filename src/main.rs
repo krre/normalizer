38a93f444414610023443bd33894b029e0d20d41
Vladimir Zarypov <krre31@gmail.com>
2022-02-05 09:44:25 +0200

    Remove dependencies from `antiq` crate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use antiq::entity::Application;
-use antiq::widget::ApplicationWindow;
+use normalizer::core::app::App;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut app = Application::new();
-
-    let mut app_window = ApplicationWindow::new(&mut app)?;
-    app_window.set_title("Normalizer");
-    app_window.set_visible(true);
-
-    app.add_window(app_window);
-
-    app.run()
+    let app = App::new()?;
+    app.run();
+    Ok(())
 }
