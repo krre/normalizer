c51aa9c32bcb922568cd6a664090f992bba65073
Vladimir Zarypov <krre31@gmail.com>
2022-02-23 07:41:36 +0200

    Add log crates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use normalizer::core::app::App;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     let app = pollster::block_on(App::new())?;
     app.run();
-    println!("+++++");
     Ok(())
 }
