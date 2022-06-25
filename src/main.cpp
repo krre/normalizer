36d6904a44a7c89922c178f2f1a3106ea37f7e75
Vladimir Zarypov <krre31@gmail.com>
2022-06-25 14:52:45 +0300

    Set ini format for settings file on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "MainWindow.h"
 #include <QApplication>
+#include <QSettings>
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+
+#ifdef Q_OS_WIN
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+#endif
+
     MainWindow mainWindow;
     mainWindow.show();
+
     return app.exec();
 }
