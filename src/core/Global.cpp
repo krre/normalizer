52e1070f192aa019f46f679b20195003d8bbefed
Vladimir Zarypov <krre31@gmail.com>
2022-06-26 17:55:19 +0300

    Refactor global workspace getter

--- src/core/Global.cpp
+++ src/core/Global.cpp
@@ -8,8 +8,7 @@ Global::Global() {
 
 QString Global::workspacePath() {
     QSettings settings;
-    QString workspace = settings.value(Const::Settings::Project::Workspace).toString();
-    return workspace.isEmpty() ? QDir::homePath() + "/" + Const::Project::WorkspaceDir : workspace;
+    return settings.value(Const::Settings::Project::Workspace, QDir::homePath() + "/" + Const::Project::WorkspaceDir).toString();
 }
 
 bool Global::restoreSession() {
