9523756894b25c1a1465300a457c318540da7f82
Vladimir Zarypov <krre31@gmail.com>
2022-09-18 08:16:29 +0300

    Add FormatComboBox component into ProjectSettings dialog

--- src/ui/dialog/ProjectSettings.cpp
+++ src/ui/dialog/ProjectSettings.cpp
@@ -1,8 +1,17 @@
 #include "ProjectSettings.h"
+#include "ui/component/FormatComboBox.h"
+#include <QtWidgets>
 
 ProjectSettings::ProjectSettings() {
     setWindowTitle(tr("Project Settings"));
 
+    formatComboBox = new FormatComboBox;
+
+    auto formLayout = new QFormLayout;
+    formLayout->addRow(tr("Format:"), formatComboBox);
+    formLayout->itemAt(0, QFormLayout::FieldRole)->setAlignment(Qt::AlignLeft);
+
+    setContentLayout(formLayout);
     resizeToWidth(600);
 }
 
--- src/ui/dialog/ProjectSettings.h
+++ src/ui/dialog/ProjectSettings.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "StandardDialog.h"
 
+class FormatComboBox;
+
 class ProjectSettings : public StandardDialog {
 public:
     ProjectSettings();
 
 private slots:
     void accept() override;
+
+private:
+    FormatComboBox* formatComboBox = nullptr;
 };
