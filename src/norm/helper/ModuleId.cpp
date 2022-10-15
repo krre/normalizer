a8d4a1979b52598e3d71e61c54271056cfcaa23d
Vladimir Zarypov <krre31@gmail.com>
2022-10-15 19:13:35 +0300

    Helper ModuleId inherits Helper class

--- src/norm/helper/ModuleId.cpp
+++ src/norm/helper/ModuleId.cpp
@@ -1,4 +1,5 @@
 #include "ModuleId.h"
+#include <QDataStream>
 
 namespace Norm::Helper {
 
@@ -6,12 +7,20 @@ ModuleId::ModuleId() {
 
 }
 
+void ModuleId::serialize(QDataStream& stream) const {
+    stream << m_moduleId;
+}
+
+void ModuleId::deserialize(QDataStream& stream) {
+    stream >> m_moduleId;
+}
+
 void ModuleId::setModuleId(Token::Id moduleId) {
-    m_module_id = moduleId;
+    m_moduleId = moduleId;
 }
 
 Token::Id ModuleId::moduleId() const {
-    return m_module_id;
+    return m_moduleId;
 }
 
 }
--- src/norm/helper/ModuleId.h
+++ src/norm/helper/ModuleId.h
@@ -1,17 +1,21 @@
 #pragma once
+#include "Helper.h"
 #include "norm/Token.h"
 
 namespace Norm::Helper {
 
-class ModuleId {
+class ModuleId : public Helper {
 public:
     ModuleId();
+
+    void serialize(QDataStream& stream) const override;
+    void deserialize(QDataStream& stream) override;
 
     void setModuleId(Token::Id moduleId);
     Token::Id moduleId() const;
 
 private:
-    Token::Id m_module_id;
+    Token::Id m_moduleId;
 };
 
 }
