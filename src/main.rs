2ab80da4f5f39288623355b99b92e16a8259f358
Vladimir Zarypov <krre31@gmail.com>
2023-02-15 16:02:01 +0200

    Setup postmortem window actions

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use antiq::core::Application;
 use normalizer::app::AppWindow;
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let app = Application::new();
-    let _ = AppWindow::new(&app);
+    let mut app = Application::new();
+    let app_window = AppWindow::new(&mut app);
     app.run();
+    app_window.finish();
     Ok(())
 }
--- src/normalizer/app/app_window.rs
+++ src/normalizer/app/app_window.rs
@@ -1,4 +1,3 @@
-use std::cell::RefCell;
 use std::rc::Rc;
 
 use antiq::core::Window;
@@ -7,30 +6,34 @@ use antiq::core::{Application, Color};
 use super::Preferences;
 
 pub struct AppWindow {
-    preferences: RefCell<Preferences>,
+    preferences: Preferences,
     window: Rc<Window>,
 }
 
 impl AppWindow {
-    pub fn new(app: &Application) -> Self {
+    pub fn new(app: &mut Application) -> Self {
         let window = app.create_window();
         window.set_title("Normalizer");
         window.set_color(Color::new(0.3, 0.3, 0.3, 1.0));
 
         Self {
-            preferences: RefCell::new(Preferences::new()),
+            preferences: Preferences::new(),
             window,
         }
     }
 
-    pub fn finish(&self) {
-        let mut prefs = self.preferences.borrow_mut();
+    pub fn finish(&self) {}
+}
 
-        (prefs.window.width, prefs.window.height) = self.window.size();
-        (prefs.window.x, prefs.window.y) = self.window.position();
+impl Drop for AppWindow {
+    fn drop(&mut self) {
+        (
+            self.preferences.window.width,
+            self.preferences.window.height,
+        ) = self.window.size();
 
-        prefs.save();
+        (self.preferences.window.x, self.preferences.window.y) = self.window.position();
 
-        println!("finish");
+        self.preferences.save();
     }
 }
