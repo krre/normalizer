60880a12b0b4f86644e43201d48afe873ecd25a2
Vladimir Zarypov <krre31@gmail.com>
2023-02-18 15:53:04 +0200

    Use logger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use antiq::core::Application;
 use normalizer::app::AppWindow;
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    env_logger::init();
+
     Application::set_organization("Norm");
     Application::set_name("Normalizer");
 
