86c25444aac612d5ffba718a17066b387c3fb67a
Vladimir Zarypov <krre31@gmail.com>
2023-04-29 19:12:38 +0300

    Save window settings by callback

--- src/normalizer/app/app_window.rs
+++ src/normalizer/app/app_window.rs
@@ -1,13 +1,14 @@
 use std::cell::RefCell;
+use std::rc::Rc;
 
-use antiq::core::layout;
 use antiq::core::window::Id;
+use antiq::core::{layout, Window};
 use antiq::core::{Application, Color};
 
 use super::Preferences;
 
 pub struct AppWindow {
-    preferences: Preferences,
+    preferences: Rc<RefCell<Preferences>>,
     window_id: Id,
     app: RefCell<Application>,
 }
@@ -17,6 +18,7 @@ impl AppWindow {
         let mut app_mut = app.borrow_mut();
         let window_id = app_mut.create_window(Box::new(layout::Box::new()));
         let mut window = app_mut.window_mut(window_id);
+
         window.set_title(&Application::name().unwrap());
         window.set_color(Color::new(0.3, 0.3, 0.3, 1.0));
 
@@ -31,24 +33,31 @@ impl AppWindow {
         drop(window);
         drop(app_mut);
 
-        Self {
-            preferences,
+        let result = Self {
+            preferences: Rc::new(RefCell::new(preferences)),
             window_id,
             app,
-        }
+        };
+
+        result.setup();
+        result
     }
 
     pub fn run(&self) {
         self.app.borrow_mut().run();
     }
-}
 
-impl Drop for AppWindow {
-    fn drop(&mut self) {
+    fn setup(&self) {
         let app_ref = self.app.borrow();
-        let window = app_ref.window_ref(self.window_id);
-        self.preferences.window.position = window.position();
-        self.preferences.window.size = window.size();
-        self.preferences.save();
+        let mut window = app_ref.window_mut(self.window_id);
+
+        let prefs = self.preferences.clone();
+        let handler = move |w: &Window| {
+            prefs.borrow_mut().window.position = w.position();
+            prefs.borrow_mut().window.size = w.size();
+            prefs.borrow().save();
+        };
+
+        window.set_drop_handler(Box::new(handler));
     }
 }
