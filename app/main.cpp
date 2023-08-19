73b4d31fdec03b47bd201c172cf42efd96ff5d82
Vladimir Zarypov <krre31@gmail.com>
2023-08-19 06:13:19 +0300

    Add Application class

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,13 @@
 #include "ui/MainWindow.h"
-#include "core/Constants.h"
+#include "core/Application.h"
 #include "core/Settings.h"
-#include <QApplication>
 
 int main(int argc, char* argv[]) {
     // Hack to fix styling with Qt 6.5 on GTK3
     qunsetenv("GNOME_DESKTOP_SESSION_ID");
     qunsetenv("XDG_CURRENT_DESKTOP");
 
-    QApplication app(argc, argv);
-    app.setOrganizationName(Const::App::Organization);
-    app.setApplicationName(Const::App::Name);
-
+    Application app(argc, argv);
     Settings::init();
 
     MainWindow mainWindow;
