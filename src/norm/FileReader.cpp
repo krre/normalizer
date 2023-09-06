646722de18805a092e2a75fecb86d1aff346f284
Vladimir Zarypov <krre31@gmail.com>
2023-09-06 08:13:10 +0300

    Read all Norm file to tokens

--- src/norm/FileReader.cpp
+++ src/norm/FileReader.cpp
@@ -3,6 +3,7 @@
 #include "Project.h"
 #include "core/Exception.h"
 #include <QFile>
+#include <QHash>
 
 namespace Norm {
 
@@ -17,14 +18,36 @@ Token* FileReader::read(const QString& filePath) {
         throw Exception("Failed to read file '{}'", filePath);
     }
 
-    QDataStream stream(&file);
+    QHash<TokenId, Token*> tokens;
+    Token* rootToken = nullptr;
 
-    while (!stream.atEnd()) {
-        Norm::TokenType type;
-        stream >> type;
+    while (!file.atEnd()) {
+        QDataStream ds(&file);
+
+        TokenSize size;
+        ds >> size;
+
+        TokenType type;
+        ds >> type;
+
+        QByteArray ba = file.read(size - sizeof(TokenType));
+        Token* token = nullptr;
+
+        if (type == TokenType::Project) {
+            token = new Project;
+        } else {
+            throw Exception("Token not found");
+        }
+
+        token->parse(ba);
+        tokens[token->id()] = token;
+
+        if (!rootToken) {
+            rootToken = token;
+        }
     }
 
-    return new Project;
+    return rootToken;
 }
 
 }
