027b6db6fba49f340f52a604c007f4b064f51ea7
Vladimir Zarypov <krre31@gmail.com>
2023-09-28 13:35:26 +0300

    Add test request for projects

--- src/ui/widget/project/ProjectTable.cpp
+++ src/ui/widget/project/ProjectTable.cpp
@@ -1,7 +1,32 @@
 #include "ProjectTable.h"
+#include "core/Settings.h"
+#include <QtNetwork>
 
 ProjectTable::ProjectTable() {
     setColumnCount(5);
-    setHorizontalHeaderLabels({ tr("Name"), tr("Templace"), tr("Description"), tr("Created time"), tr("Updated time") });
+    setHorizontalHeaderLabels({ tr("Name"), tr("Template"), tr("Description"), tr("Created time"), tr("Updated time") });
     setSelectionBehavior(QAbstractItemView::SelectItems);
+    networkAccessManager.reset(new QNetworkAccessManager);
+
+    load();
+}
+
+void ProjectTable::load() {
+    QString host = Settings::value<Server::Host>();
+    quint64 port = Settings::value<Server::Port>();
+
+    QNetworkRequest request(QUrl(QString("http://%1:%2/projects").arg(host).arg(port)));
+    QNetworkReply* reply = networkAccessManager->get(request);
+
+    connect(reply, &QNetworkReply::finished, [=] {
+        reply->deleteLater();
+    });
+
+    connect(reply, &QNetworkReply::errorOccurred, [=] (QNetworkReply::NetworkError code) {
+        qWarning().noquote() << code;
+    });
+
+    connect(reply, &QNetworkReply::readyRead, [=] {
+        qDebug() << reply->readAll();
+    });
 }
--- src/ui/widget/project/ProjectTable.h
+++ src/ui/widget/project/ProjectTable.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <QTableWidget>
 
+class QNetworkAccessManager;
+
 class ProjectTable : public QTableWidget {
 public:
     ProjectTable();
+
+private:
+    void load();
+
+    QScopedPointer<QNetworkAccessManager> networkAccessManager;
 };
