7e03e00c124df91851bc266eb797e83898d63188
Vladimir Zarypov <krre31@gmail.com>
2023-11-07 18:02:39 +0200

    Add tests for LoginDialog

---
+++ tests/dialog/login/tst_login.cpp
@@ -0,0 +1,37 @@
+#include "ui/dialog/account/LoginDialog.h"
+#include "tests/common/TestNetworkManager.h"
+#include <QTest>
+#include <QLineEdit>
+
+constexpr auto Email = "admin@example.com";
+constexpr auto Password = "qwerty";
+
+class TestLogin : public QObject {
+    Q_OBJECT
+private slots:
+    void validData();
+};
+
+void TestLogin::validData() {
+    TestNetworkManager networkManager;
+    LoginDialog loginDialog(&networkManager);
+
+    QLineEdit* emailLineEdit = static_cast<QLineEdit*>(loginDialog.focusWidget());
+    emailLineEdit->setText(Email);
+
+    QTest::keyClick(&loginDialog, Qt::Key_Tab);
+
+    QLineEdit* passwordLineEdit = static_cast<QLineEdit*>(loginDialog.focusWidget());
+    passwordLineEdit->setText(Password);
+
+    QTest::keyClick(&loginDialog, Qt::Key_Tab);
+
+    loginDialog.accept();
+
+    QCOMPARE(networkManager.m_user.email, Email);
+    QCOMPARE(networkManager.m_user.password, Password);
+}
+
+QTEST_MAIN(TestLogin)
+
+#include "tst_login.moc"
