8a98f0a82a0216cf32822966421b00a83d7dafbc
Vladimir Zarypov <krre31@gmail.com>
2023-12-11 08:33:49 +0200

    Move all settings operations from Settings class to FileSettingsStorage

--- src/manager/settings/FileSettingsStorage.cpp
+++ src/manager/settings/FileSettingsStorage.cpp
@@ -1,36 +1,72 @@
 #include "FileSettingsStorage.h"
-#include "core/Settings.h"
+#include <QSettings>
 
 FileSettingsStorage::FileSettingsStorage() {
-    Settings::init();
 }
 
 void FileSettingsStorage::setMainWindow(const MainWindow& mainWindow) {
-    Settings::setValue<::MainWindow::Geometry>(mainWindow.geometry);
-    Settings::setValue<::MainWindow::State>(mainWindow.state);
+    QSettings settings;
+    settings.beginGroup("MainWindow");
+
+    settings.setValue("geometry", mainWindow.geometry);
+    settings.setValue("state", mainWindow.state);
+
+    settings.endGroup();
 }
 
 SettingsStorage::MainWindow FileSettingsStorage::mainWindow() const {
-    return MainWindow(Settings::value<::MainWindow::Geometry>(), Settings::value<::MainWindow::State>());
+    MainWindow result;
+
+    QSettings settings;
+    settings.beginGroup("MainWindow");
+
+    result.geometry = settings.value("geometry").toByteArray();
+    result.state = settings.value("state").toByteArray();
+
+    settings.endGroup();
+
+    return result;
 }
 
 bool FileSettingsStorage::containsGeometry() const {
-    return Settings::contains<::MainWindow::Geometry>();
+    QSettings settings;
+    return settings.contains("MainWindow/geometry");
 }
 
 void FileSettingsStorage::setServerAddress(const ServerAddress& serverAddress) {
-    Settings::setValue<Server::Host>(serverAddress.host);
-    Settings::setValue<Server::Port>(serverAddress.port);
+    QSettings settings;
+    settings.beginGroup("Server");
+
+    settings.setValue("host", serverAddress.host);
+    settings.setValue("port", serverAddress.port);
+
+    settings.endGroup();
 }
 
 SettingsStorage::ServerAddress FileSettingsStorage::serverAddress() const {
-    return ServerAddress(Settings::value<Server::Host>(), Settings::value<Server::Port>());
+    ServerAddress result;
+
+    QSettings settings;
+    settings.beginGroup("Server");
+
+    result.host = settings.value("host", "api.norm.dev").toByteArray();
+    result.port = settings.value("port", 80).toInt();
+
+    settings.endGroup();
+
+    return result;
 }
 
 void FileSettingsStorage::setAccount(const Account& account) {
-    Settings::setValue<::Account::Token>(account.token);
+    QSettings settings;
+    settings.setValue("Account/token", account.token);
 }
 
 SettingsStorage::Account FileSettingsStorage::account() const {
-    return Account(Settings::value<::Account::Token>());
+    QSettings settings;
+
+    Account result;
+    result.token = settings.value("Account/token").toString();
+
+    return result;
 }
