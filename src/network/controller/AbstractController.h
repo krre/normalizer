6fae00a8f0d3e3b8f5e7c35c6820fb8a3869cd02
Vladimir Zarypov <krre31@gmail.com>
2023-12-15 17:19:32 +0200

    Remove commented code

--- src/network/controller/AbstractController.h
+++ src/network/controller/AbstractController.h
@@ -1,25 +1,9 @@
 #pragma once
-#include <QJsonObject>
+#include <QString>
 
 class NetworkManager;
 
 namespace Controller {
-
-// template <typename T>
-// class Request {
-// public:
-//     QJsonObject serialize()
-
-// private:
-//     T data;
-// };
-
-// template <typename T>
-// class Response {
-
-// private:
-    // T data;
-// };
 
 class AbstractController {
 public:
@@ -27,12 +11,6 @@ public:
 
     virtual QString name() const = 0;
     NetworkManager* networkManager() const;
-
-    // Async::Task<QString> create(const User& user);
-    // Async::Task<void> update(const User& user);
-    // Async::Task<User> getOne();
-    // Async::Task<User> getList();
-    // Async::Task<void> remove();
 
 private:
     NetworkManager* m_networkManager = nullptr;
