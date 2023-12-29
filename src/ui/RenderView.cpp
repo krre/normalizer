af5e16a20065a89b07e2791e36a34913c433bfec
Vladimir Zarypov <krre31@gmail.com>
2023-12-29 09:58:20 +0200

    Remove Qt3D dependency

--- src/ui/RenderView.cpp
+++ src/ui/RenderView.cpp
@@ -1,15 +1,9 @@
 #include "RenderView.h"
 #include <QtWidgets>
-#include <Qt3DExtras>
 
 RenderView::RenderView(Id projectId) {
-    Qt3DExtras::Qt3DWindow* view = new Qt3DExtras::Qt3DWindow();
-    view->defaultFrameGraph()->setClearColor(QColor(QRgb(0x4d4d4f)));
-    view->renderSettings()->setRenderPolicy(Qt3DRender::QRenderSettings::OnDemand);
-
     auto layout = new QVBoxLayout(this);
     layout->setContentsMargins(0, 0, 0, 0);
-    layout->addWidget(QWidget::createWindowContainer(view));
 
     setVisible(false);
 }
