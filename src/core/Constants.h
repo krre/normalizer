c117cd20ba1a2cbf3a9da197fb1e56ed6b127607
Vladimir Zarypov <krre31@gmail.com>
2024-01-01 06:06:20 +0200

    Update copyright date

--- src/core/Constants.h
+++ src/core/Constants.h
@@ -7,7 +7,7 @@ namespace App {
     constexpr auto Name = "Normalizer";
     constexpr auto Version = "0.1.0";
     constexpr auto URL = "https://github.com/krre/normalizer";
-    constexpr auto CopyrightYear = "2022-2023";
+    constexpr auto CopyrightYear = "2022-2024";
     constexpr auto BuildDate = __DATE__;
     constexpr auto BuildTime = __TIME__;
 }
