40a6411c95d08b677888d27848439de996877dc5
Vladimir Zarypov <krre31@gmail.com>
2024-02-25 11:05:36 +0200

    Remove location setting from 'New Project' dialog

--- src/ui/dialog/project/NewProjectDialog.cpp
+++ src/ui/dialog/project/NewProjectDialog.cpp
@@ -11,6 +11,7 @@ NewProjectDialog::NewProjectDialog(Settings* settings) : m_settings(settings) {
     connect(m_nameLineEdit, &QLineEdit::textChanged, this, &NewProjectDialog::enableOkButton);
 
     m_descriptionLineEdit = new QLineEdit;
+    m_workspaceBrowseLayout = new BrowseLayout(m_settings->projectLocation().workspace);
 
     m_targetComboBox = new QComboBox;
     m_targetComboBox->addItems({
@@ -18,30 +19,15 @@ NewProjectDialog::NewProjectDialog(Settings* settings) : m_settings(settings) {
         Project::targetString(Project::Target::Library)
     });
 
-    m_locationComboBox = new QComboBox;
-    m_locationComboBox->addItems({
-        Project::locationString(Project::Location::Local),
-        Project::locationString(Project::Location::Remote)
-    });
+    auto formLayout = new QFormLayout;
+    formLayout->addRow(tr("Name:"), m_nameLineEdit);
+    formLayout->addRow(tr("Description:"), m_descriptionLineEdit);
+    formLayout->addRow(tr("Workspace:"), m_workspaceBrowseLayout);
+    formLayout->addRow(tr("Target:"), m_targetComboBox);
 
-    connect(m_locationComboBox, &QComboBox::currentIndexChanged, this, &NewProjectDialog::switchLocation);
+    formLayout->itemAt(formLayout->indexOf(m_targetComboBox))->setAlignment(Qt::AlignLeft);
 
-    m_workspaceBrowseLayout = new BrowseLayout(m_settings->projectLocation().workspace);
-    m_hostLineEdit = new QLineEdit(m_settings->projectLocation().host);
-
-    m_formLayout = new QFormLayout;
-    m_formLayout->addRow(tr("Name:"), m_nameLineEdit);
-    m_formLayout->addRow(tr("Description:"), m_descriptionLineEdit);
-    m_formLayout->addRow(tr("Target:"), m_targetComboBox);
-    m_formLayout->addRow(tr("Location:"), m_locationComboBox);
-    m_formLayout->addRow(tr("Workspace:"), m_workspaceBrowseLayout);
-    m_formLayout->addRow(tr("Host:"), m_hostLineEdit);
-
-    m_formLayout->itemAt(m_formLayout->indexOf(m_targetComboBox))->setAlignment(Qt::AlignLeft);
-    m_formLayout->itemAt(m_formLayout->indexOf(m_locationComboBox))->setAlignment(Qt::AlignLeft);
-
-    setContentLayout(m_formLayout, false);
-    switchLocation();
+    setContentLayout(formLayout, false);
     resizeToWidth(500);
 
     m_nameLineEdit->setFocus();
@@ -49,24 +35,9 @@ NewProjectDialog::NewProjectDialog(Settings* settings) : m_settings(settings) {
 }
 
 void NewProjectDialog::accept() {
-    Settings::ProjectLocation projectLocation;
-    projectLocation.workspace = m_workspaceBrowseLayout->text();
-    projectLocation.host = m_hostLineEdit->text();
-
-    m_settings->setProjectLocation(projectLocation);
     StandardDialog::accept();
 }
 
 void NewProjectDialog::enableOkButton() {
     buttonBox()->button(QDialogButtonBox::Ok)->setEnabled(!m_nameLineEdit->text().isEmpty());
-}
-
-void NewProjectDialog::switchLocation() {
-    if (m_locationComboBox->currentIndex() == int(Project::Location::Local)) {
-        m_formLayout->setRowVisible(m_hostLineEdit, false);
-        m_formLayout->setRowVisible(m_workspaceBrowseLayout, true);
-    } else {
-        m_formLayout->setRowVisible(m_workspaceBrowseLayout, false);
-        m_formLayout->setRowVisible(m_hostLineEdit, true);
-    }
 }
--- src/ui/dialog/project/NewProjectDialog.h
+++ src/ui/dialog/project/NewProjectDialog.h
@@ -4,7 +4,6 @@
 class Settings;
 class BrowseLayout;
 
-class QFormLayout;
 class QLineEdit;
 class QComboBox;
 
@@ -18,16 +17,12 @@ public slots:
 
 private slots:
     void enableOkButton();
-    void switchLocation();
 
 private:
     Settings* m_settings = nullptr;
 
-    QFormLayout* m_formLayout = nullptr;
     QLineEdit* m_nameLineEdit = nullptr;
     QLineEdit* m_descriptionLineEdit = nullptr;
-    QLineEdit* m_hostLineEdit = nullptr;
     QComboBox* m_targetComboBox = nullptr;
-    QComboBox* m_locationComboBox = nullptr;
     BrowseLayout* m_workspaceBrowseLayout = nullptr;
 };
