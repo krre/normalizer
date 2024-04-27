bf9818c640f2f1ccef7c8e971286b2a815f41470
Vladimir Zarypov <krre31@gmail.com>
2024-04-27 06:25:36 +0300

    Remove using Qt3D

--- src/ui/editor/3d/View3D.cpp
+++ src/ui/editor/3d/View3D.cpp
@@ -1,14 +1,7 @@
 #include "View3D.h"
 #include "Window3D.h"
 #include <QtWidgets>
-#include <Qt3DExtras>
 
 View3D::View3D(NodeManager* nodeManager) : m_nodeManager(nodeManager) {
-    auto view = new Window3D(nodeManager);
-    view->defaultFrameGraph()->setClearColor(QColor(QRgb(0x4d4d4f)));
-    view->renderSettings()->setRenderPolicy(Qt3DRender::QRenderSettings::OnDemand);
 
-    auto layout = new QVBoxLayout(this);
-    layout->setContentsMargins(0, 0, 0, 0);
-    layout->addWidget(QWidget::createWindowContainer(view));
 }
