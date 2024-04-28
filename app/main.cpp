e32ab4c2330ad79edcf4f84bee7042f3e76f895f
Vladimir Zarypov <krre31@gmail.com>
2024-04-28 09:59:19 +0300

    Move QML files to 'qml' directory

--- app/main.cpp
+++ app/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("app", &app);
-    engine.load(QUrl("qrc:/Main.qml"));
+    engine.load(QUrl("qrc:/qml/Main.qml"));
 
     if (engine.rootObjects().isEmpty()) {
         return EXIT_FAILURE;
