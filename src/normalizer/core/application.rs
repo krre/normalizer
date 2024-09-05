100dc4680d123e8919c1aa97fac0f5cd73af1fc3
Vladimir Zarypov <krre31@gmail.com>
2024-09-05 06:26:55 +0300

    Add save and restore window maximized state

--- src/normalizer/core/application.rs
+++ src/normalizer/core/application.rs
@@ -46,16 +46,19 @@ impl ApplicationHandler for Application {
         let mut attrs = Window::default_attributes();
 
         if self.preferences.load() {
-            attrs = attrs
-                .with_position(winit::dpi::PhysicalPosition::new(
-                    self.preferences.window.x,
-                    self.preferences.window.y,
-                ))
-                .with_inner_size(winit::dpi::PhysicalSize::new(
-                    self.preferences.window.width,
-                    self.preferences.window.height,
-                ));
-            // .with_maximized(self.preferences.window.is_maximized);
+            if self.preferences.window.is_maximized {
+                attrs = attrs.with_maximized(self.preferences.window.is_maximized);
+            } else {
+                attrs = attrs
+                    .with_position(winit::dpi::PhysicalPosition::new(
+                        self.preferences.window.x,
+                        self.preferences.window.y,
+                    ))
+                    .with_inner_size(winit::dpi::PhysicalSize::new(
+                        self.preferences.window.width,
+                        self.preferences.window.height,
+                    ));
+            }
         }
 
         attrs = attrs.with_title(NAME.to_string());
@@ -64,21 +67,26 @@ impl ApplicationHandler for Application {
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        let window = self.window.as_ref().unwrap();
+
         match event {
             WindowEvent::CloseRequested => {
+                let pref_window = &mut self.preferences.window;
+                pref_window.is_maximized = window.is_maximized();
+
+                if !window.is_maximized() {
+                    pref_window.width = window.inner_size().width;
+                    pref_window.height = window.inner_size().height;
+
+                    pref_window.x = window.outer_position().as_ref().unwrap().x;
+                    pref_window.y = window.outer_position().as_ref().unwrap().y;
+                }
+
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                self.window.as_ref().unwrap().request_redraw();
+                window.request_redraw();
                 self.renderer.render();
-            }
-            WindowEvent::Moved(pos) => {
-                self.preferences.window.x = pos.x;
-                self.preferences.window.y = pos.y;
-            }
-            WindowEvent::Resized(size) => {
-                self.preferences.window.width = size.width;
-                self.preferences.window.height = size.height;
             }
             _ => (),
         }
