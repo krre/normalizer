661263bc7a7bef72c7b1b08a3a17d0d26d266025
Vladimir Zarypov <krre31@gmail.com>
2025-02-23 05:42:13 +0200

    Add `env_logger` crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use normalizer::core::Application;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
     let app = Application::new()?;
     app.run()?;
     Ok(())
