2a8fc0b0c6194cd8b2d07d5142111120eddd7941
Vladimir Zarypov <krre31@gmail.com>
2025-11-30 13:48:15 +0200

    Add getters for project attributes

--- src/project/Project.cpp
+++ src/project/Project.cpp
@@ -7,6 +7,18 @@ Project::Project(QObject* parent) : QObject(parent) {
     m_normProcess->setProgram(CliTool);
 }
 
+QString Project::name() const {
+    return m_name;
+}
+
+QString Project::directory() const {
+    return m_directory;
+}
+
+Project::Target Project::target() const {
+    return m_target;
+}
+
 void Project::create(const QString& name, const QString& directory, Target target) {
     QDir dir;
     dir.mkpath(directory);
@@ -14,4 +26,8 @@ void Project::create(const QString& name, const QString& directory, Target targe
     m_normProcess->setWorkingDirectory(directory);
     m_normProcess->setArguments({ "create", name, target == Target::Application ? "--app" : "--lib" });
     m_normProcess->start();
+
+    m_name = name;
+    m_directory = directory;
+    m_target = target;
 }
--- src/project/Project.h
+++ src/project/Project.h
@@ -14,8 +14,16 @@ public:
 
     Project(QObject* parent);
 
+    QString name() const;
+    QString directory() const;
+    Target target() const;
+
     void create(const QString& name, const QString& directory, Target target);
 
 private:
+    QString m_name;
+    QString m_directory;
+    Target m_target;
+
     QProcess* m_normProcess = nullptr;
 };
