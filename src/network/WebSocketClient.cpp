00f180b542d59402e2b9a665dd4b0051c89c9751
Vladimir Zarypov <krre31@gmail.com>
2025-12-08 09:26:04 +0200

    Add websocket support

---
+++ src/network/WebSocketClient.cpp
@@ -0,0 +1,23 @@
+#include "WebSocketClient.h"
+#include <QWebSocket>
+
+WebSocketClient::WebSocketClient(int port) : m_port(port) {
+    m_webSocket = new QWebSocket;
+    m_webSocket->setParent(this);
+
+    QObject::connect(m_webSocket, &QWebSocket::connected, this, [this] {
+        emit stateChanged(State::Connected);
+    });
+
+    QObject::connect(m_webSocket, &QWebSocket::disconnected, this, [this] {
+        emit stateChanged(State::Disconnected);
+    });
+
+    QObject::connect(m_webSocket, &QWebSocket::binaryMessageReceived, this, [this] (const QByteArray& message) {
+        emit messageReceived(message);
+    });
+}
+
+void WebSocketClient::connect() {
+    m_webSocket->open(QUrl(QString("ws://127.0.0.1:%1").arg(m_port)));
+}
---
+++ src/network/WebSocketClient.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <QObject>
+
+class QWebSocket;
+
+class WebSocketClient : public QObject {
+    Q_OBJECT
+public:
+    enum class State {
+        Connected,
+        Disconnected
+    };
+
+    WebSocketClient(int port);
+    void connect();
+
+signals:
+    void stateChanged(WebSocketClient::State state);
+    void messageReceived(const QByteArray& message);
+
+private:
+    QWebSocket* m_webSocket = nullptr;
+    int m_port = 0;
+};
