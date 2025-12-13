733dc19046fd6ee52878cfdd4bd801ff387357a9
Vladimir Zarypov <krre31@gmail.com>
2025-12-13 10:09:13 +0200

    Add Network::sendMessage()` method

--- src/network/Network.cpp
+++ src/network/Network.cpp
@@ -1,9 +1,14 @@
 #include "Network.h"
+#include "WebSocketClient.h"
 
 namespace Api {
 
 Network::Network(WebSocketClient* webSocketClient, QObject* parent) : QObject(parent), m_webSocketClient(webSocketClient) {
 
+}
+
+void Network::sendMessage(const QByteArray& message) {
+    m_webSocketClient->sendMessage(message);
 }
 
 }
--- src/network/Network.h
+++ src/network/Network.h
@@ -9,6 +9,8 @@ class Network : public QObject {
 public:
     Network(WebSocketClient* webSocketClient, QObject* parent = nullptr);
 
+    void sendMessage(const QByteArray& message);
+
 private:
     WebSocketClient* m_webSocketClient = nullptr;
 };
