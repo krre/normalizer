305b6475c29b73919374425b7860ad519612af69
Vladimir Zarypov <krre31@gmail.com>
2026-01-01 09:43:39 +0200

    Update copyright last year to 2026

--- src/core/Application.h
+++ src/core/Application.h
@@ -9,7 +9,7 @@ public:
     static constexpr auto WorkspaceName = "Norm Projects";
     static constexpr auto Version = PROJECT_VERSION;
     static constexpr auto Url = "https://github.com/krre/normalizer";
-    static constexpr auto Years = "2015-2025";
+    static constexpr auto Years = "2015-2026";
     static constexpr auto BuildDate = __DATE__;
     static constexpr auto BuildTime = __TIME__;
 
