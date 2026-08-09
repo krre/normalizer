e1e835a17744ae07b5ef27c5dc984e1ff61c12a4
Vladimir Zarypov <krre31@gmail.com>
2026-08-09 15:57:46 +0300

    Remove allocator parameter from `angie3d.runApp()` function

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,6 @@
-const std = @import("std");
 const angie3d = @import("angie3d");
 const Normalizer = @import("Normalizer.zig");
 
 export fn main() void {
-    angie3d.runApp(Normalizer, std.heap.wasm_allocator);
+    angie3d.runApp(Normalizer);
 }
